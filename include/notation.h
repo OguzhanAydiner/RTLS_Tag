cdf005a5f3ef6ea9a7fdd03ace3efd5f7d80c49c
Oğuzhan Aydıner <oguzaydin206@gmail.com>
2023-10-15 14:11:48 +0300

    First commit of Tag

---
+++ include/notation.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdint.h>
+
+
+#define	U8 							uint8_t
+#define	U16							uint16_t
+#define U32							uint32_t
+#define BOOL						bool
+#define TRUE 						true
+#define FALSE						false
